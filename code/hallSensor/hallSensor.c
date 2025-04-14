8c8730871bf0932160f71e68944a7ccd6aee30c1
romeo <rayzer7731@gmail.com>
2025-04-14 14:25:05 +0200

    neovim clangd options set

--- code/hallSensor/hallSensor.c
+++ code/hallSensor/hallSensor.c
@@ -12,10 +12,9 @@
 
 #include "hallSensor.h"
 
-static void		ft_itoaptr(uint16_t conv, char *ptr)
-{
-	uint16_t	convTmp;
-	uint8_t		convLen;
+static void ft_itoaptr(uint16_t conv, char* ptr) {
+	uint16_t convTmp;
+	uint8_t	 convLen;
 
 	convLen = 1;
 	convTmp = conv;
@@ -24,48 +23,49 @@ static void		ft_itoaptr(uint16_t conv, char *ptr)
 	ptr[convLen--] = '\0';
 	if (conv == 0)
 		ptr[convLen] = '0';
-	while (conv > 0)
-	{
+	while (conv > 0) {
 		ptr[convLen--] = (conv % 10) + '0';
 		conv /= 10;
 	}
 }
 
-int		main(void)
-{
-	uint16_t	conv;
-	char		digital[6];	//	Max value = 65535 + '\0'
+int main(void) {
+	uint16_t conv;
+	char	 digital[6];  //	Max value = 65535 + '\0'
 
 	//	Init
 
-		//	Set port
-	DDRF &= ~(1u << DDF0);		//	Set pin A0 as input (Port F 0)
-	PORTF &= ~(1u << PORTF0);	//	Disable pull up resistor
+	//	Set port
+	DDRF &= ~(1u << DDF0);	   //	Set pin A0 as input (Port F 0)
+	PORTF &= ~(1u << PORTF0);  //	Disable pull up resistor
 
 	uart_init();
 
-		//	Init ADC
-	ADMUX |= (1u << REFS0);		//	Set AVcc as reference
-	ADCSRA |= (1u << ADEN);		//	Enable ADC
-	ADCSRA |= (1u << ADPS0);	//	Set prescaler to 128
-	ADCSRA |= (1u << ADPS1);	//	Set prescaler to 128
-	ADCSRA |= (1u << ADPS2);	//	Set prescaler to 128
+	//	Init ADC
+	ADMUX |= (1u << REFS0);	  //	Set AVcc as reference
+	ADCSRA |= (1u << ADEN);	  //	Enable ADC
+	ADCSRA |= (1u << ADPS0);  //	Set prescaler to 128
+	ADCSRA |= (1u << ADPS1);  //	Set prescaler to 128
+	ADCSRA |= (1u << ADPS2);  //	Set prescaler to 128
 
-	DIDR0 |= (1u << ADC1D) | (1u << ADC2D) | (1u << ADC3D) | (1u << ADC4D) | (1u << ADC5D) | (1u << ADC6D) | (1u << ADC7D);							//	Disable unused ADC inputs
-	DIDR1 |= (1u << ADC8D) | (1u << ADC9D) | (1u << ADC10D) | (1u << ADC11D) | (1u << ADC12D) | (1u << ADC13D) | (1u << ADC14D) | (1u << ADC14D);	//	Disable unused ADC inputs
+	DIDR0 |= (1u << ADC1D) | (1u << ADC2D) | (1u << ADC3D) | (1u << ADC4D) | (1u << ADC5D) | (1u << ADC6D) |
+			 (1u << ADC7D);	 //	Disable unused ADC inputs
+	DIDR1 |= (1u << ADC8D) | (1u << ADC9D) | (1u << ADC10D) | (1u << ADC11D) | (1u << ADC12D) | (1u << ADC13D) |
+			 (1u << ADC14D) | (1u << ADC14D);  //	Disable unused ADC inputs
 
 	//	Dummy run for ADC initialisation
-	ADCSRA |= (1u << ADSC);			//	Start conversion
-	_delay_ms(10);					//	First conversion may take some time
-	while (ADCSRA & (1u << ADSC));	//	Wait for ADC to finish conversion
-	conv = ADC;						//	Read conversion
+	ADCSRA |= (1u << ADSC);	 //	Start conversion
+	_delay_ms(10);			 //	First conversion may take some time
+	while (ADCSRA & (1u << ADSC))
+		;		 //	Wait for ADC to finish conversion
+	conv = ADC;	 //	Read conversion
 
 	//	Loop
 
-	for (;;)
-	{
-		ADCSRA |= (1u << ADSC);			//	Start conversion
-		while (ADCSRA & (1u << ADSC));	//	Wait for conversion to finish
+	for (;;) {
+		ADCSRA |= (1u << ADSC);	 //	Start conversion
+		while (ADCSRA & (1u << ADSC))
+			;  //	Wait for conversion to finish
 		conv = ADC;
 		ft_itoaptr(conv, digital);
 		uart_printstrnl(digital);
