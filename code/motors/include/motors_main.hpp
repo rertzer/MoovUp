7a7a20ff6aca7c0ea8af84dcc8fc778113365d30
romeo <rayzer7731@gmail.com>
2025-05-13 12:22:56 +0200

    motor code

--- code/motors/include/motors_main.hpp
+++ code/motors/include/motors_main.hpp
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define TIMER1_TOP 312
+
 void motors_init();
 void motors_run();
 
--- code/motors/src/main.cpp
+++ code/motors/src/main.cpp
@@ -3,6 +3,26 @@
 #include "uart_init.h"
 #include "uart_printstrnl.h"
 
+int	 duty = 1;
+bool ascending = true;
+
+ISR(TIMER0_OVF_vect) {
+	if (ascending) {
+		if (duty >= 100) {
+			duty = 99;
+			ascending = false;
+		} else
+			++duty;
+	} else {
+		if (duty <= 0) {
+			duty = 1;
+			ascending = true;
+		} else
+			--duty;
+	}
+	OCR1A = duty;
+}
+
 int main() {
 	motors_init();
 	while (42) {
@@ -15,6 +35,23 @@ int main() {
 
 void motors_init() {
 	uart_init();
+
+	/* set port D2 as output */
+	DDRD |= (1 << DDD2);
+
+	/*timer 0 settings mode 15*/
+	TCCR0A = 0b11000011;
+	TCCR0B = 0b00001101;
+	OCR0A = 78;
+	TIMSK0 = (1 << TOIE0);
+
+	/* timer 1 settings mode 14*/
+	TCCR1A = (1 << COM1A1) | (1 << WGM11);
+	TCCR1B = 0b00011101;
+	OCR1A = duty;
+	ICR1 = TIMER1_TOP;
+
+	sei();
 }
 
 void motors_run() {
